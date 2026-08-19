4af9bb2203ee9861a59068223e8bbbc4587022b1
daniel <danielmendezdemr222@gmail.com>
2026-08-18 22:06:26 -0500

    agrego perido de oscilacion

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@ int main() {
     double frecuencia = (1.0 / (2.0 * 3.14159265359)) * 
                         std::sqrt(k / masa);
 
+    double periodo = 1.0 / frecuencia;
     std::cout << "Masa: " << masa << " kg\n";
     std::cout << "Rigidez: " << k << " N/m\n";
     std::cout << "Frecuencia natural: " << frecuencia << " Hz\n";
+    std::cout << "Periodo: " << periodo << " s\n";
 
     return 0;
 }
